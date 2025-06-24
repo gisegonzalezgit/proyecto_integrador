1f11403b1f83f4ed764880d3c7e6b98254395f83
Mpaljacar <mperezv@isciii.es>
2025-06-24 11:39:22 +0200

    modifico archivo en paciente Juan

--- archivo.py
+++ archivo.py
@@ -1,7 +1,7 @@
 #diccionario de datos nombre, apellido, edad, ciudad
 {
     "nombre": ["Juan", "Ana", "Pedro", "Maria"],    
-    "apellido": ["Perez", "Gomez", "Lopez", "Martinez"],
-    "edad": [28, 34, 23, 45],
-    "ciudad": ["Madrid", "Barcelona", "Valencia", "Sevilla"]
+    "apellido": ["Vazquez", "Gomez", "Lopez", "Martinez"],
+    "edad": [99, 34, 23, 45],
+    "ciudad": ["Vigo", "Barcelona", "Valencia", "Sevilla"]
 }
