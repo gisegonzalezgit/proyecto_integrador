c67747a96ca783d11e0906d8afad8b013af9da4f
gisegonzalezgit <gise.gonzalez144@gmail.com>
2025-06-24 04:29:32 -0300

    master - mi primer commit con gitignore

---
+++ archivo.py
@@ -0,0 +1,7 @@
+#diccionario de datos nombre, apellido, edad, ciudad
+{
+    "nombre": ["Juan", "Ana", "Pedro", "Maria"],    
+    "apellido": ["Perez", "Gomez", "Lopez", "Martinez"],
+    "edad": [28, 34, 23, 45],
+    "ciudad": ["Madrid", "Barcelona", "Valencia", "Sevilla"]
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# documentacion de proyecto de prueba
+
+## Probaremos GitFlow en equipo
+
+<ul>
+  <li>prueba
