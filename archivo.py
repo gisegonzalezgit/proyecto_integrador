10e1e7e6bd65285bcc75b767358357fa04792c5f
micrayala <ayalasu.ruben@gmail.com>
2025-06-24 11:38:23 +0200

    Añado entrada Bernardo en dicc

--- archivo.py
+++ archivo.py
@@ -1,7 +1,7 @@
 #diccionario de datos nombre, apellido, edad, ciudad
 {
-    "nombre": ["Juan", "Ana", "Pedro", "Maria"],    
-    "apellido": ["Perez", "Gomez", "Lopez", "Martinez"],
-    "edad": [28, 34, 23, 45],
-    "ciudad": ["Madrid", "Barcelona", "Valencia", "Sevilla"]
+    "nombre": ["Juan", "Ana", "Pedro", "Maria", "Bernardo"],    
+    "apellido": ["Perez", "Gomez", "Lopez", "Martinez", "Fernandez"], 
+    "edad": [28, 34, 23, 45, 16],
+    "ciudad": ["Madrid", "Barcelona", "Valencia", "Sevilla", "Cadiz"]
 }
